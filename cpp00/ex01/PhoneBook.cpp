46a1c3063b990edbfe091569d5afba1907644352
Abdessalam Mounach <mounach-@hotmail.com>
2023-01-18 12:47:33 +0100

    chore: Add contact to the phone book

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,9 +1,20 @@
-#include  "phoneBook.hpp"
+#include  "PhoneBook.hpp"
+#include "Contact.hpp"
 
-
-void phoneBook::ADD(void)
+void PhoneBook::ADD(void)
 {
-	str firstName, lastName, nickName, phoneNumber, darkestSecret;
-	
+	Contact  newContact;
 
+	fill_contact("Enter first name", newContact.firstName);
+	fill_contact("Enter last name", newContact.lastName);
+	fill_contact("Enter nick name", newContact.nickName);
+	fill_contact("Enter phone number", newContact.phoneNumber);
+	fill_contact("Enter darkest secret", newContact.darkestSecret);
+
+	for (int i = 0; i < 8; i++)
+	{
+		if(contacts[i].firstName.empty())
+			contacts[i] = newContact;
+		break ;
+	}
 }
