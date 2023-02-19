357abc839fd3e7f28ed8bc735844bb4dd8caab1f
Abdessalam Mounach <mounach-@hotmail.com>
2023-02-19 19:14:53 +0100

    chore: add main

--- cpp04/ex00/main.cpp
+++ cpp04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/17 18:52:38 by amounach          #+#    #+#             */
-/*   Updated: 2023/02/17 20:20:55 by amounach         ###   ########.fr       */
+/*   Updated: 2023/02/19 19:11:30 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,16 @@
 
 int main()
 {
+    const Animal *meta = new Animal();
+    const Animal *j = new Dog();
+    const Animal *i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
+    i->makeSound(); // will output the cat sound! 
+    j->makeSound();
+    meta->makeSound();
+    delete meta;
+    delete i;
+    delete j;
     return (0);
 }
