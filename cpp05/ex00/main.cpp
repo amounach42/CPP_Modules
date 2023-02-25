99e0ae99ed07141058dca6ebf28490e51c1a8131
Abdessalam Mounach <mounach-@hotmail.com>
2023-02-25 22:55:21 +0100

    chore: add tests

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 23:53:37 by amounach          #+#    #+#             */
-/*   Updated: 2023/02/25 20:38:40 by amounach         ###   ########.fr       */
+/*   Updated: 2023/02/25 22:54:50 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,25 +18,45 @@ int main()
     {
         /* code */
         Bureaucrat a("Bureaucrat1", 149);
-        std::cout << a.getGrade() << "\n";
-        a.incrementGrade();
+        std::cout << "Name: " << a.getName() << "\n";
+        std::cout << "Grade: " << a.getGrade() << "\n";
         a.decrementGrade();
         a.decrementGrade();
-        std::cout << a.getGrade() << "\n";
-        // std::cout << a.getName() << "\n";
+        std::cout << "Grade: " << a.getGrade() << "\n";
+        std::cout << a;
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
+    std::cout << "-----------------------------------" << std::endl;
 
     try
     {
-        /* code */
+        Bureaucrat a("amounach", 5);
+        std::cout << a;
+        std::cout << "Name: " << a.getName() << "\n";
+        std::cout << "Grade before increment: " << a.getGrade() << "\n";
+        a.incrementGrade();
+        std::cout << "Grade after increment: " << a.getGrade() << "\n";
     }
-    catch(const std::exception& e)
+    catch (const std::exception &e)
     {
         std::cerr << e.what() << '\n';
     }
-    
+    std::cout << "-----------------------------------" << std::endl;
+
+    try
+    {
+        Bureaucrat a("ayoub", 0);
+        std::cout << a;
+        std::cout << "Name: " << a.getName() << "\n";
+        std::cout << "Grade before increment: " << a.getGrade() << "\n";
+        a.incrementGrade();
+        std::cout << "Grade after increment: " << a.getGrade() << "\n";
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
