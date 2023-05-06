a12bc30a69ce601654797bb10c9b7aca9030b48d
Abdessalam Mounach <mounach-@hotmail.com>
2023-05-06 19:47:06 +0100

    chore: read database file

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,9 +6,48 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 22:09:06 by amounach          #+#    #+#             */
-/*   Updated: 2023/03/14 22:09:16 by amounach         ###   ########.fr       */
+/*   Updated: 2023/05/06 19:46:25 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange()
+{
+}
+BitcoinExchange::~BitcoinExchange()
+{
+}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &obj)
+{
+    *this = obj;
+}
+// BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchage &obj)
+// {
+//     return (*this);
+// }
+
+std::string BitcoinExchange::getFileContent(std::string fileName)
+{
+    std::string content;
+    std::string tmp;
+    bool is_last;
+    
+    std::ifstream file(fileName);
+    if (!file.is_open())
+    {
+        std::cerr << "Error: unable to open file" << std::endl;
+        exit(0);
+    }
+    std::getline(file, tmp);
+    while (!file.eof())
+    {
+        std::getline(file, tmp);
+        std::cout << tmp << std::endl;
+        exit(0);
+        is_last = file.eof();
+        content = content + tmp + (is_last ? "" : "\n");
+    }
+    std::cout << content << std::endl;
+    return (content);
+}
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: amounach <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 20:28:29 by amounach          #+#    #+#             */
-/*   Updated: 2023/03/14 22:09:37 by amounach         ###   ########.fr       */
+/*   Updated: 2023/05/06 15:50:32 by amounach         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 #define BTC_HPP
 
 #include <iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
 
 class BitcoinExchange{
     private:
         std::string content;
+        std::map<std::string, float> data;
     public:
         BitcoinExchange();
         ~BitcoinExchange();
+        BitcoinExchange(const BitcoinExchange &obj);
+        // BitcoinExchange &operator=(const BitcoinExchange &obj);
+        std::string getFileContent(std::string fileName);
+        
 };
 #endif
